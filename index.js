639ddaf6d24c4c64e628eb92648db0c061f587d5
ifyour <ifyour@outlook.com>
2023-11-04 14:21:08 +0800

    feat: trigger deployment

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ import { http } from "@ampt/sdk";
 
 const app = fastify();
 
-app.get("/", (_request, reply) => {
-  reply.send({ hello: "world" });
-});
-
 app.get("/translate", (_request, reply) => {
   reply.send({ hello: "ifyour" });
 });
